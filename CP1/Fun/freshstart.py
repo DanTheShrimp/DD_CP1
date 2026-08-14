52b69a951a09c2fcbf1cc3694d638f04820f5c92
DanTheShrimp <daniel.delong@ucas-edu.net>
2026-08-14 08:59:57 -0600

    added comments and some stuff

--- CP1/Fun/freshstart.py
+++ CP1/Fun/freshstart.py
@@ -1,6 +1,6 @@
-import sys, time
+import sys,time,random
 
-stages=[
+stages=[ #There are seven stages, number 0 is the first one, 6 is the last
     [" ___    ",
     " |  |    ",
     "\\o/ |    ",
@@ -57,11 +57,36 @@ stages=[
     "    |    ",
     "____|____"],
 ]
-loop_helper=0
-while True:
-    for item in stages[loop_helper]:
-        print(item)
-    print(" ")
-    if loop_helper==6:
-         break
-    loop_helper+=1
+
+def tester_function(): #this function exists so I can test if the stages print properly
+    loop_helper=0
+    while True:
+        for item in stages[loop_helper]:
+            print(item)
+        print(" ")
+        if loop_helper==6:
+            break
+        loop_helper+=1
+
+# \/ list of words, if I change the number of words I have to update the random number generator \/
+words=["Food","Computer","Somewhere","Attacked","Disoriented","Alright","Chili","Bridge","Programmer","Number"]
+
+def start():
+    #choosing the word, assigning it to a variable, and getting its length
+    chosen_word=random.randint(0,9) #update this if the number of words change
+    chosen_word=str(words[chosen_word])
+    chosen_word_length=len(chosen_word)
+
+    current_stage=[]
+    current_stage.append(stages[0]) #setting the first stage so I don't have to later
+
+    while True:
+        for item in current_stage:
+            print(item)
+        break
+        
+
+        
+
+start()
+
