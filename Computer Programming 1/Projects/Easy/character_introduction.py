f220678cdbca244b92764eb6a4960e12e92d93df
DanTheShrimp <daniel.delong@ucas-edu.net>
2026-08-19 09:01:24 -0600

    charockter intrudoction

---
+++ Computer Programming 1/Projects/Easy/character_introduction.py
@@ -0,0 +1,8 @@
+#Daniel DeLong, Character Introduction
+
+name="Chubs"
+age="79"
+job="Professional Napper"
+home="Detritus"
+
+print(f"Hello, I'm {name}! I'm {age} years old (probably), I'm a {job}, and my home is {home}!")
