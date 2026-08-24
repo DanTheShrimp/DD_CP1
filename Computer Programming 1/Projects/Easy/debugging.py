87d6ac035194972bf89a42f6fbe36ecb658f4c25
DanTheShrimp <daniel.delong@ucas-edu.net>
2026-08-24 09:12:15 -0600

    finished straight bird, started slantup bird, also finished the debugging assignment

---
+++ Computer Programming 1/Projects/Easy/debugging.py
@@ -0,0 +1,13 @@
+#Daniel DeLong, Fix the Code!
+
+#old code
+# name = "Alex"
+# age = 16
+
+# print("My name is " + nam + " and I am " + str(age) + " years old."
+
+name="Alex" #took away the spaces because they aren't necessary
+age="16" #we can set it to be a string here to save on characters later
+
+print("My name is "+name+" and I am "+age+" years old.") #name variable was misspelled, removed the str(), and closed the parenthesis
+#also I removed all unnecessary spaces, we need EVERY SINGLE BIT TO BE OPTIMIZED
--- Fun/flappy_bird.py
+++ Fun/flappy_bird.py
@@ -5,7 +5,16 @@ def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 bird_straight=[
-    ""
+    "  __  ",
+    " -  o>",
+    "  ̅ ̅   "
+]
+bird_slantup=[
+    "  /--_",
+    " |  o/",
+    "  ̅ ̅   "
 ]
 for item in bird_straight:
+    print(item)
+for item in bird_slantup:
     print(item)
