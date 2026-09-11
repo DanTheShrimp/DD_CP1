d6f24b405ad3cb9dfde4fdbf6dae424183be946f
DanTheShrimp <daniel.delong@ucas-edu.net>
2026-09-11 09:05:43 -0600

    1 in 75 for going to a new line

--- Fun/hacking_the_matrix.py
+++ Fun/hacking_the_matrix.py
@@ -4,7 +4,7 @@ import time,random
 while True:
     number_to_print=random.randint(0,1)
     print(number_to_print,end="")
-    do_new_line=random.randint(1,50)
+    do_new_line=random.randint(1,75)
     if do_new_line==1:
         print("")
     time.sleep(0.0001)
