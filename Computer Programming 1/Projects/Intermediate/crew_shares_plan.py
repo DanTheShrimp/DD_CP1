820e2e947f34fc3259b077d6a83a64573c208ae0
DanTheShrimp <daniel.delong@ucas-edu.net>
2026-09-17 09:18:20 -0600

    finished crew_shares_plan

---
+++ Computer Programming 1/Projects/Intermediate/crew_shares_plan.py
@@ -0,0 +1,16 @@
+#Daniel DeLong, Crew Shares Pseudocode
+"""
+GET number of pirates from user
+GET each name for the pirates
+STORE the names in a list
+GENERATE random number for number of money we got
+CALCULATE
+    Yondu gets 13% (0.13)
+    Peter gets 11% of what's left (0.11(0.87))
+    Divide the rest among the rest of the crew (the rest of it divided by user input)
+    Round to two decimal places at EACH point
+DISPLAY Yondu's name and share
+DISPLAY Peter's name and share
+LOOP
+    DISPLAY the next pirate's name and share
+"""
