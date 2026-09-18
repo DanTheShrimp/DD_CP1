feea81cf2fdcde61fffa72036054d9270255deab
DanTheShrimp <daniel.delong@ucas-edu.net>
2026-09-18 09:44:25 -0600

    updated my pseudocode, made progress on crew_shares

--- Computer Programming 1/Projects/Intermediate/crew_shares.py
+++ Computer Programming 1/Projects/Intermediate/crew_shares.py
@@ -7,7 +7,7 @@ def typer(text):
     print("")
 
 while True:
-    typer("My system memory has been wiped. Please input the number of crew on Yondu's ship.")
+    typer("My system memory has been wiped. Please input the number of crew on Yondu's ship, not including Yondu and Peter.")
     try:
         number_of_crew=int(input(""))
     except:
@@ -34,8 +34,20 @@ while number_of_crew>0:
     number_of_crew-=1
     time.sleep(0.75)
 
-credits=random.randint(2000,8000)
-yondu_share=round(credits*0.13,2)
-credits-=yondu_share
-peter_share=round(credits*0.11,2)
-credits-=peter_share
+total_credits=random.randint(2000,8000)
+yondu_share=round(total_credits*0.13,2)
+remaining_credits=total_credits-yondu_share
+peter_share=round(remaining_credits*0.11,2)
+remaining_credits-=peter_share
+
+number_of_crew=loop_helper-1
+credits_per_crew=round((remaining_credits/number_of_crew)+2,2)
+typer(f"The most recent mission earned you {total_credits}")
+typer(f"Yondu's share: {yondu_share+credits_per_crew} credits.")
+typer(f"Peter's share: {peter_share+credits_per_crew} credits.")
+
+loop_helper=1
+while number_of_crew>0:
+    typer(f"{list_of_names[loop_helper]}'s share: {credits_per_crew} credits.")
+    loop_helper+=1
+    number_of_crew-=1
--- Computer Programming 1/Projects/Intermediate/crew_shares_plan.py
+++ Computer Programming 1/Projects/Intermediate/crew_shares_plan.py
@@ -1,14 +1,19 @@
 #Daniel DeLong, Crew Shares Pseudocode
 """
-GET number of pirates from user
+GET number of pirates from user, excluding Yondu and Peter
 GET each name for the pirates
 STORE the names in a list
 GENERATE random number for number of money we got
 CALCULATE
-    Yondu gets 13% (0.13)
-    Peter gets 11% of what's left (0.11(0.87))
-    Divide the rest among the rest of the crew (the rest of it divided by user input)
-    Round to two decimal places at EACH point
+    Yondu gets 13%
+    ROUND to two decimal places
+
+    Peter gets 11% of what's left
+    ROUND to two decimal places
+
+    Divide the rest among the rest of the crew, plus two (the rest of it divided by user input, now including Yondu and Peter)
+    ROUND to two decimal places
+
 DISPLAY Yondu's name and share
 DISPLAY Peter's name and share
 LOOP
