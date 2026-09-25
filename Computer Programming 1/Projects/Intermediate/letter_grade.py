91ffbe01202530833a766067926ac4caf4856f2e
Shrimp <daniel.delong@ucas-edu.net>
2026-09-25 17:40:08 +0000

    finished comments on letter_grade

--- Computer Programming 1/Projects/Intermediate/letter_grade.py
+++ Computer Programming 1/Projects/Intermediate/letter_grade.py
@@ -28,27 +28,33 @@ def typer(text):
         time.sleep(0.05)
     print("")
 
+def panic(): #if something goes wrong we panic
+    typer("AAHHHHHHHH")
+    sys.exit()
+
 while True:
     typer("How many classes do you have?")
     try:
-        num_of_classes=int(input(""))
+        num_of_classes=int(input("")) #ask for an integer input
     except:
         time.sleep(0.75)
-        typer("Please put in a valid number.")
+        typer("Please put in a valid number.") #if they don't put in an integer we tell them to put it one
     else:
         break
 
+#set some helpful lists
 list_percentages=[]
 letter_grade_list=[]
 
-copy_of_num=num_of_classes
-loop_helper=1
+copy_of_num=num_of_classes #i don't want to change the original number
+loop_helper=1 #i <3 loop_helper, it's always so helpful
 time.sleep(0.25)
 while copy_of_num>0:
     stringed_loop_helper=str(loop_helper) #convert loop_helper to a string
     last_number=stringed_loop_helper[len(stringed_loop_helper)-1] #get the last letter in loop_helper
     while True:
         time.sleep(0.75)
+        #make sure that grammar is correct, even if 11, 12, and 13 will show up weird. i'm 100% sure that they do not have more than 10 classes
         if last_number=="1":
             typer(f"What is the percentage grade of your {stringed_loop_helper}st class?.")
         elif last_number=="2":
@@ -58,20 +64,21 @@ while copy_of_num>0:
         else:
                 typer(f"What is the percentage grade of your {stringed_loop_helper}th class?.")
         try:
-            grade_input=float(input(""))
+            grade_input=float(input("")) #get a float/decimal input
         except:
             time.sleep(0.75)
-            typer("Please put in a valid number.")
+            typer("Please put in a valid decimal number.") #get angry at them
         else:
              break
-    list_percentages.append(grade_input)
-    copy_of_num-=1
-    loop_helper+=1
+    list_percentages.append(grade_input) #if the input passes then we add it to list_percentages
+    copy_of_num-=1 #subtract one from copy_of_num
+    loop_helper+=1 #add one to loop_helper
 
-copy_of_num=num_of_classes
-loop_helper=1
+copy_of_num=num_of_classes #set this again
+loop_helper=1 #and this
 time.sleep(0.25)
 while copy_of_num>0:
+    #big boy brain checking
     for item in list_percentages:
         if item>=93:
             letter_grade="A"
@@ -98,11 +105,12 @@ while copy_of_num>0:
         elif item<60:
             letter_grade="F"
         else:
-            break
-        letter_grade_list.append(letter_grade)
+            break #if they SOMEHOW make this if-elif train break, break the loop
+        letter_grade_list.append(letter_grade) #add the letter grade to letter_grade_list
     stringed_loop_helper=str(loop_helper) #convert loop_helper to a string
     last_number=stringed_loop_helper[len(stringed_loop_helper)-1] #get the last letter in loop_helper
     time.sleep(0.75)
+    #grammar!
     if last_number=="1":
         typer(f"The letter grade of your {stringed_loop_helper}st class is {letter_grade_list[loop_helper-1]}.")
     elif last_number=="2":
@@ -111,11 +119,13 @@ while copy_of_num>0:
         typer(f"The letter grade of your {stringed_loop_helper}rd class is {letter_grade_list[loop_helper-1]}.")
     else:
         typer(f"The letter grade of your {stringed_loop_helper}th class is {letter_grade_list[loop_helper-1]}.")
+    #the same exact thing, wow i wonder if the loops are literally the exact same! that would be such a coincidence!
     copy_of_num-=1
     loop_helper+=1
 
-grade_sum=sum(list_percentages)
-average_grade=round(grade_sum/num_of_classes,2)
+grade_sum=sum(list_percentages) #get the sum of all values in the list list_percentages
+average_grade=round(grade_sum/num_of_classes,2) #divide it by the number of classes and round to two decimal places
+#more checking
 if average_grade>=93:
     letter_grade="A"
 elif average_grade>=90:
@@ -140,5 +150,9 @@ elif average_grade>=60:
     letter_grade="D-"
 elif average_grade<60:
     letter_grade="F"
+else:
+    panic() #if they SOMEHOW make this if-elif train break, panic
+
+#tell them their average grade percentage and average grade letter
 time.sleep(1)
 typer(f"Your average percentage grade is {average_grade}, so your average letter grade is {letter_grade}.")
